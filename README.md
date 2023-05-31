98af80f58d7d63c04ee89c5b8d8b229a577eb7ad
walterdyna <alexdyna@gmail.com>
2023-05-31 17:50:46 -0300

    getElementByID

---
+++ README.md
@@ -0,0 +1,11 @@
+# getElementById - JavaScript
+
+
+----------------------------------- getElementById -----------------------------------
+
+getElementById --> Tras um elemento pelo ID
+
+
+
+A função getElementById é um método do JavaScript que permite buscar um elemento específico em uma página da web com base em seu atributo "id". O método recebe o ID como parâmetro e retorna uma referência para o elemento correspondente. É importante verificar se o elemento foi encontrado, pois o método retorna null se nenhum elemento com o ID especificado for encontrado.
+
---
+++ script.js
@@ -0,0 +1,26 @@
+/* 
+
+Buscar o elemento no HTML...
+
+document --> HTML
+
+getElementById --> Tras um elemento pelo ID
+
+getElementByClassName --> Tras TODOS os elementos com essa classe
+
+getElementByTagName --> Tras TODOS os elementos com essa TAG
+
+gatElementByName --> Tras TODOS os elementos com esse NAME
+
+
+querySelector --> Tras UM elemneto, O PRIMEIRO que encontra
+
+querySelectorAll --> Tras TODOS os elementos que encontrar
+
+*/
+
+const input = document.getElementById ("main-input")
+console.log(input)
+
+const elements = document.getElementsByClassName("paragraph-js")
+console.log(elements)
